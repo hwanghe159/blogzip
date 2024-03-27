8139edb361b88c8cef95a0cad86e87b6af8f85e0
황준호 <hwanghe159@naver.com>
2024-03-27 23:55:09 +0900

    react sample

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 function App() {
+
+  const [message, setMessage] = useState([])
+
+  useEffect(() => {
+    Test();
+  })
+
+  function Test() {
+    axios.get(`/api/v1/test`)
+    .then((res) => {
+      setMessage(res.data);
+    })
+    .catch((error) => {
+      console.log("API call 실패")
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {message}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
